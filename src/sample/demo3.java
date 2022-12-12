0315d25ea847f722bd4685088b1c2be566c8fbb0
Dell <Dell@DESKTOP-HH8MK94>
2022-12-12 23:17:14 +0530

    [Akshay]: Check demo3

---
+++ src/sample/demo3.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class demo3
+{
+public static void main(String[] args) 
+{
+	 String s1 = new String("Velocity");	
+	    String s2 = new String("Velocity");
+	    
+	    //I want result as true and also false do it by using string methods
+	    
+	    System.out.println("don't sleep do this task now");
+}
+}
